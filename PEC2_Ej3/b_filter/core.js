dcbf4674ac431fefa452d290e3ad421b96780d9a
Carlos <carloscolomer99@gmail.com>
2023-04-06 16:35:41 +0200

    realización b_filter

--- PEC2_Ej3/b_filter/core.js
+++ PEC2_Ej3/b_filter/core.js
@@ -1,17 +1,27 @@
 function onlyEven(array) {
-  // your code here
+  let result = array.filter(number => number % 2 === 0);
+
+  return result;
 }
 
 function onlyOneWord(array) {
-  // your code here
+  let result = array.filter(word => word.indexOf(" ") === -1);
+
+  return result;
 }
 
 function positiveRowsOnly(array) {
-  // your code here
+  const result = array.filter(numbers => numbers.every(number => number > 0));
+
+  return result;
 }
 
 function allSameVowels(array) {
-  // your code here
+    let result = array.filter(word => {
+      let vowelToMatch = word.match(/[aeiou]/g)[0];
+      return word.match(/[aeiou]/g).every(vowel => vowel === vowelToMatch);
+    });
+    return result;
 }
 
 module.exports = {
