4ab0626a16cbd22364ec862b1433ecd21123bd26
Carlos <carloscolomer99@gmail.com>
2023-04-09 15:14:29 +0200

    realización ejercicios every

--- PEC2_Ej3/d_every/core.js
+++ PEC2_Ej3/d_every/core.js
@@ -2,14 +2,31 @@
 // are even numbers.
 
 function allEven(input) {
-  return input;
+
+  const isEven = (currentValue) => currentValue % 2 === 0;
+
+  return input.every(isEven);
+
+   
 }
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType(input) {
-  return input;
+
+  let type = "";
+  
+  return input.every((item) =>{
+    if(type === ""){
+      type = typeof item;
+      return true;
+    }
+    else {
+      return type === typeof item
+    }
+      
+  });
 }
 
 // Check to see if every element in the matrix is
@@ -17,14 +34,43 @@ function allSameType(input) {
 // greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  return input.every((item) =>{
+    if(Array.isArray(item)){
+      return item.every((num) => num > 0);
+    }
+      
+  });
 }
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels(input) {
-  return input;
+
+  let vowel = "";
+
+  return input.every((item) =>{
+
+    if(typeof item === 'string' ){
+      const match = item.match(/[aeiou]/i);
+      if (match) {
+        vowel = match[0];
+        const result = item.split('').every((letter) => {
+          const matchLetter = letter.match(/[aeiou]/i);
+          if (!matchLetter) {
+            return true;
+          }
+
+          return matchLetter[0] === vowel;
+        });
+
+        return result;
+      }
+      return false;
+    }
+      
+  });
+
 }
 
 module.exports = {
