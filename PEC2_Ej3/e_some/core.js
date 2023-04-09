ecc0678fbba9ca5f2fbcc663266e913a8f286355
Carlos <carloscolomer99@gmail.com>
2023-04-09 15:31:06 +0200

    realizado el ejercicio some

--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -2,28 +2,38 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  const ten = (element) => element > 10;
+
+  return input.some(ten);
+
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+
+  const ten = (element) => element.length > 10;
+
+  return input.some(ten);
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  const esVerdadero = (element) => element === true;
+
+  return input.some(isTrue => isTrue.some(esVerdadero));
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  const withLost = (element) => element.includes("Lost");
+
+  return input.some(withLost);
 };
 
 module.exports = {
