524918c05711b0f77e1e7c6497e45b6464b8436c
Carlos <carloscolomer99@gmail.com>
2023-04-09 12:49:43 +0200

    realizados los ejercicions c_reduce

--- PEC2_Ej3/c_reduce/core.js
+++ PEC2_Ej3/c_reduce/core.js
@@ -1,17 +1,47 @@
 function sum(array) {
-  // your code here
+  return  array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
 }
 
 function productAll(array) {
-  // your code here
+  return  array.reduce(function(a, b) {
+      return a.concat(b);
+    }).reduce(function(x, y) {
+      return x * y;
+    });
+  
 }
 
 function objectify(array) {
-  // your code here
+  return array.reduce(function(obj, arr) {
+    obj[arr[0]] = arr[1];
+    return obj;
+  }, {});
+  
 }
 
 function luckyNumbers(array) {
-  // your code here
+  let frase = "Your lucky numbers are: ";
+ 
+  const resultado = array.reduce((acumula, numero, index) =>{
+      
+      if((array.length - 1) === index){
+          return acumula + "and " + numero; 
+      }
+      else{
+        if(acumula === array[index-1]){
+          acumula + ",";
+         return acumula.toString() + ', ' + numero.toString() + ', ';
+        }else{
+            return acumula.toString() + numero.toString() + ', ';
+        }
+      }
+    
+  });
+
+  return frase + resultado;
+
 }
 
 module.exports = {
